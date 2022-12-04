5b0e9bad5634ae95118485537271a484d9799c67
[mick lito] <[micklito.dev@gmail.com]>
2022-12-04 17:55:36 -0500

    updated footer and css cdn for footer

--- public/footer.js
+++ public/footer.js
@@ -3,7 +3,80 @@
 
 
 function footerPassToHtml() {
-    return document.write(`<footer class="footer" style="color:rgba(0, 0, 0, 0.697)">Email: Micklito.dev@gmail.com | Phone: 470-356-5173 | <a href="contactus.html">
-    <input type="button" class="btn btn-secondary" value="Contact Us" />
-  </a> </footer>
+    return document.write(`   <footer class="bg text-center text-white" style="background-color: rgb(255, 255, 216 )">
+    <!-- Grid container -->
+    <div class="container p-4 pb-0">
+      <!-- Section: Social media -->
+      <section class="mb-4">
+        <!-- Facebook -->
+        <a
+          class="btn text-white btn-floating m-1"
+          style="background-color: #3b5998;"
+          href="#!"
+          role="button"
+          ><i class="fab fa-facebook-f"></i
+        ></a>
+  
+        <!-- Twitter -->
+        <a
+          class="btn text-white btn-floating m-1"
+          style="background-color: #55acee;"
+          href="#!"
+          role="button"
+          ><i class="fab fa-twitter"></i
+        ></a>
+  
+        <!-- Google -->
+        <a
+          class="btn text-white btn-floating m-1"
+          style="background-color: #dd4b39;"
+          href="#!"
+          role="button"
+          ><i class="fab fa-google"></i
+        ></a>
+  
+        <!-- Instagram -->
+        <a
+          class="btn text-white btn-floating m-1"
+          style="background-color: #ac2bac;"
+          href="#!"
+          role="button"
+          ><i class="fab fa-instagram"></i
+        ></a>
+  
+        <!-- Linkedin -->
+        <a
+          class="btn text-white btn-floating m-1"
+          style="background-color: #0082ca;"
+          href="#!"
+          role="button"
+          ><i class="fab fa-linkedin-in"></i
+        ></a>
+        <!-- Github -->
+        <a
+          class="btn text-white btn-floating m-1"
+          style="background-color: #333333;"
+          href="#!"
+          role="button"
+          ><i class="fab fa-github"></i
+        ></a>
+      </section>
+      <!-- Section: Social media -->
+    </div>
+    
+    <div style="color:black">
+    Email: Micklito.dev@gmail.com | Phone: 470-356-5173 | <a href="contactus.html">
+        <input type="button" class="btn btn-secondary" value="Contact Us" />
+      </a>
+    </div>
+    
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+      © 2020 Copyright:
+      <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+    </div>
+  
+  </footer>
     `)}
+
+
+   
