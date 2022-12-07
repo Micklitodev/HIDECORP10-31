28e18ad97a626cfc62e31350fc4cc7b07a2b99aa
[mick lito] <[micklito.dev@gmail.com]>
2022-12-07 12:08:49 -0500

    updated footer.js and fixed css bugs

--- public/footer.js
+++ public/footer.js
@@ -8,15 +8,7 @@ function footerPassToHtml() {
     <div class="container p-4 pb-0">
       <!-- Section: Social media -->
       <section class="mb-4">
-        <!-- Facebook -->
-        <a
-          class="btn text-white btn-floating m-1"
-          style="background-color: #3b5998;"
-          href="#!"
-          role="button"
-          ><i class="fab fa-facebook-f"></i
-        ></a>
-  
+
         <!-- Twitter -->
         <a
           class="btn text-white btn-floating m-1"
@@ -30,20 +22,10 @@ function footerPassToHtml() {
         <a
           class="btn text-white btn-floating m-1"
           style="background-color: #dd4b39;"
-          href="#!"
+          href="https://www.google.com"
           role="button"
           ><i class="fab fa-google"></i
         ></a>
-  
-        <!-- Instagram -->
-        <a
-          class="btn text-white btn-floating m-1"
-          style="background-color: #ac2bac;"
-          href="#!"
-          role="button"
-          ><i class="fab fa-instagram"></i
-        ></a>
-  
         <!-- Linkedin -->
         <a
           class="btn text-white btn-floating m-1"
@@ -56,12 +38,11 @@ function footerPassToHtml() {
         <a
           class="btn text-white btn-floating m-1"
           style="background-color: #333333;"
-          href="#!"
+          href="https://github.com/Micklitodev"
           role="button"
           ><i class="fab fa-github"></i
         ></a>
       </section>
-      <!-- Section: Social media -->
     </div>
     
     <div style="color:black">
